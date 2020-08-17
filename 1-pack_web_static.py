ce2f4230a9f53a06eec172a765b741952d8e5c16
VMP1312 <victor.paz1312@hotmail.com>
2020-08-17 14:23:32 -0500

    Fix

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -7,11 +7,10 @@ from fabric.api import local
 
 def do_pack():
     """Generates a .tgz archive from the contents of the web_static folder"""
-    try:
-        time = datetime.now().strftime('%Y%m%d%H%M%S')
-        local('mkdir -p versions')
-        local('tar -cvzf "versions/web_static_{}.tgz" web_static'.format(time))
-        path = "versions/web_static_{}.tgz" web_static'.format(time)
-        return path
-    except failed:
-        return None
+    time = datetime.now().strftime('%Y%m%d%H%M%S')
+    local('mkdir -p versions')
+    local('tar -cvzf "versions/web_static_{}.tgz" web_static'.format(time))
+    path = "versions/web_static_{}.tgz" web_static'.format(time)
+    if command.succeeded:
+        return fn
+    return None
