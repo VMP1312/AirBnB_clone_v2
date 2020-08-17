ab000ff0ebd953ed2ad2c36aee788fc2bbd6fe00
VMP1312 <victor.paz1312@hotmail.com>
2020-08-17 14:59:12 -0500

    Fix

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -11,8 +11,8 @@ env.hosts = ['35.185.63.9', '54.242.105.247']
 def do_deploy(archive_path):
     """Distributes an archive to your web servers."""
 
-    rout = "/data/web_static/releases/"
     dirc = archive_path.replace(".tgz", "").replace("versions/", "")
+    rout = "/data/web_static/releases/"
 
     if not path.exists(archive_path):
         return False
@@ -20,39 +20,44 @@ def do_deploy(archive_path):
     ret = True
 
     tmpfolder = put(archive_path, '/tmp/')
+
     if tmpfolder.failed:
         ret = False
 
-    dest = run('mkdir -p {}{}/'.format(rout, dirc))
+    dest = run('mkdir -p {}' + dirc + '/'.fotmat(rout))
 
     if dest.failed:
         ret = False
 
-    unpack = run('tar -xzf /tmp/{1}.tgz -C {0}{1}/'.format(rout, dirc))
+    unpack = run('tar -xzf /tmp/' + dirc + '.tgz' +
+                 ' -C {}' + dirc + '/'.fotmat(rout))
 
     if unpack.failed:
         ret = False
 
-    clean = run('rm /tmp/{}'.format(dirc))
+    clean = run('rm /tmp/' + dirc + '.tgz')
 
     if clean.failed:
         ret = False
 
-    move = run('mv {0}{1}/web_static/* {0}{1}/'.format(rout, dirc))
+    move = run('mv {}' + dirc + '/web_static/* {}' + dirc + '/'.fotmat(rout))
 
     if move.failed:
         ret = False
 
-    cleanfolder = run('rm -rf {}{}/web_static'.format(rout, dirc))
+    cleanfolder = run('rm -rf {}' + dirc + '/web_static'.fotmat(rout))
 
     if cleanfolder.failed:
         ret = False
 
     rmold = run('rm -rf /data/web_static/current')
+
     if rmold.failed:
         ret = False
 
-    new = run('ln -s {}{}/ /data/web_static/current'.format(rout, dirc))
+    new = run('ln -sf {}' + dirc +
+              '/' + ' /data/web_static/current'.fotmat(rout))
+
     if new.failed:
         ret = False
 
